1e365185b16f089049eab2f046055b12dca6a74b
Stan Rosenbaum <ForeverTangent@gmail.com>
2017-10-05 14:23:56 -0400

    Broken for some reasons.

---
+++ BasicStructure/BasicStructure/StructureSwift.m
@@ -0,0 +1,9 @@
+//
+//  StructureSwift.m
+//  BasicStructure
+//
+//  Created by Stanley Rosenbaum on 10/5/17.
+//  Copyright © 2017 Rosenbaum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
